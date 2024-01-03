033945facdf893f4f4e7eb1613f54400c878e2d9
Willian <williansantos.developer@gmail.com>
2024-01-03 22:43:00 +0000

    First-project-Hamburg-web

---
+++ script.js
@@ -0,0 +1,28 @@
+const fotoPrincipal = document.querySelector(".img-principal")
+const imgOne = document.querySelector(".one")
+const imgTwo = document.querySelector(".two")
+const imgThree = document.querySelector(".three")
+const imgFour = document.querySelector(".four")
+const imgFive = document.querySelector(".five")
+
+function btnOrange(){
+    alert("Pedido esta a caminho")
+}
+
+function one() {
+    fotoPrincipal.src = "./assets/hamburg-1.png"
+}
+function two() {
+    fotoPrincipal.src = "./assets/hamburg-2.png"
+}
+function three() {
+    fotoPrincipal.src = "./assets/hamburg-4.png"
+}
+function four() {
+    fotoPrincipal.src = "./assets/hamburg-3.png"
+}
+function five() {
+    fotoPrincipal.src = "./assets/hamburg-5.png"
+}
+
+
